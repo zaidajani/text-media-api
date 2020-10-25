5c2b2eba1adcba93f3ee45efd008bb57d1ae27ad
Zaid <zaidajani2007@gmail.com>
2020-10-25 17:29:57 +0530

    first message

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const dotenv = require('dotenv').config();
+
+app.use(express.json());
+
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to mongodb"));
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timeAndDate: {
+    type: String,
+    required: true
+  }
+});
+
+const Data = mongoose.model("data", schema);
+
+app.get("/", async (req, res) => {
+  const found = await Data.find();
+  res.send(found);
+});
+
+app.post("/", async (req, res) => {
+  const data = new Data({
+    name: req.body.name,
+    content: req.body.content,
+    timeAndDate: req.body.timeAndDate
+  });
+  await data.save();
+  res.send(data);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
